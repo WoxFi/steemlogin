d022b1b90a8275c2597b328384b299af56cd15b9
opnetworksys <97328046+opnetworksys@users.noreply.github.com>
2022-07-22 06:43:16 +0545

    df

--- src/components/Confirmation.vue
+++ src/components/Confirmation.vue
@@ -2,7 +2,7 @@
   <div class="flash flash-success mb-4 text-center">
     Your transaction is on the way! Here is the ID of the transaction:
     <p>{{id}}</p>
-    <a :href="`https://steemscan.com/transaction/${id}`" target="_blank">Click here to view this transaction on Steemscan.com</a>
+    <a :href="`https://steemsdb.io/transaction/${id}`" target="_blank">Click here to view this transaction on Steemdb.io</a>
     <button v-if="isExtension" type="submit" class="btn btn-large btn-success mt-4" @click="close">
       Close
     </button>
